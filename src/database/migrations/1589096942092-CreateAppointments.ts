4d79ef9c340aff8676abb2a566c1ff9dbb30f3c6
Tony Costa <tonyblack10@gmail.com>
2020-05-10 05:33:14 -0300

    repositório do TypeORM

--- src/database/migrations/1589096942092-CreateAppointments.ts
+++ src/database/migrations/1589096942092-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589096942092
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -9,14 +10,12 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  public execute({ date, provider }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInTheSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInTheSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -24,10 +23,12 @@ class CreateAppointmentService {
       throw new Error('This appointment is already booked!');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
