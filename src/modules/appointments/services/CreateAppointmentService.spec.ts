e4fc4499ebee179ef481a42985514a90b648d302
Tony Costa <tonyblack10@gmail.com>
2020-05-17 01:41:50 -0300

    criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toEqual(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '83f5cb7b-67df-4157-94a9-ac2237aa7368',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe(
+      '83f5cb7b-67df-4157-94a9-ac2237aa7368',
+    );
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+
+  // });
 });
