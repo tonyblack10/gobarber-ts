8c67f6fa2d9b0a496b74c822750301602f43401d
Tony Costa <tonyblack10@gmail.com>
2020-05-16 23:45:19 -0300

    refatorando módulo de usuários

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return this.ormRepository.findOne(id);
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.ormRepository.findOne({ where: { email } });
+  }
+
+  public async create(data: ICreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create(data);
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    return this.ormRepository.save(user);
+  }
+}
+
+export default UsersRepository;
