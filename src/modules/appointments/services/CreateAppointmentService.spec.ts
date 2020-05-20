a37bcf986e125f0cd7cf3724508defbf9301ac22
Tony Costa <tonyblack10@gmail.com>
2020-05-20 01:49:18 -0300

    recuperação de senha

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -33,7 +33,7 @@ describe('CreateAppointment', () => {
       provider_id: '83f5cb7b-67df-4157-94a9-ac2237aa7368',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '83f5cb7b-67df-4157-94a9-ac2237aa7368',
---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,22 @@
+import { uuid } from 'uuidv4';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+import IUserTokensRepository from '../IUserTokensRepository';
+
+export default class FakeUserTokensRepository implements IUserTokensRepository {
+  private userToken: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userToken.push(userToken);
+
+    return userToken;
+  }
+}
