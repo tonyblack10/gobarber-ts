1e313d234792daa9b0566e8ea032170c2ea29605
Tony Costa <tonyblack10@gmail.com>
2020-05-24 02:15:09 -0300

    excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -50,9 +50,12 @@ class ListProviderDayAvailabilityService {
         appointment => getHours(appointment.date) === hour,
       );
 
+      const currentDate = new Date(Date.now());
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
   }
